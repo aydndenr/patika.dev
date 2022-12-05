d97a306061fb85f865dc3ffacc92d58a2bc3024b
Aydın Demir <aydndenr@gmail.com>
2022-12-05 13:14:43 +0300

    Create Vki.java
    VKI

---
+++ Vki.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Vki {
+	public static void main(String[] args) {
+		
+		Scanner scan= new Scanner(System.in);
+		double boy,kilo,vki;
+		
+	System.out.println("Boyunuzu giriniz mt=");
+	boy=scan.nextDouble();
+	
+	System.out.println("Kilonuzu giriniz kg=");
+	kilo=scan.nextDouble();
+
+	
+		System.out.println("VKİ= "+kilo/Math.pow(boy, 2));
+		 }
+
+}
