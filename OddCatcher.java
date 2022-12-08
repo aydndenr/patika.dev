97a098468dc140797a4f655ef60a6431f88cf1fb
Aydın Demir <aydndenr@gmail.com>
2022-12-09 02:03:02 +0300

    Update OddCatcher.java

--- OddCatcher.java
+++ OddCatcher.java
@@ -8,21 +8,15 @@ public class OddCatcher {
 		Scanner scan = new Scanner(System.in);
 		int odd, sum = 0;
 		System.out.println("Sayı girmeye başlayın tek girene kadar devam");
-		odd = scan.nextInt();
-		if (odd % 2 != 0) {
-			System.out.println("girilen sayı tektir " + odd);
-			return;
-		}
-		while (odd % 2 == 0) {
-			System.out.println(odd % 4 == 0 ? odd + "dörde bölündü teprikler" : odd + "dörde bölünmes");
-			sum = odd % 4 == 0 ? (odd + sum) : sum;
-			System.out.println(" tek girene kadar devam");
+		do {
 			odd = scan.nextInt();
+			System.out.println(odd % 4 == 0 ? odd + " dörde bölündü teprikler " : odd + " dörde bölünmes ");
+			sum = odd % 4 == 0 ? (odd + sum) : sum;
 			if (odd % 2 != 0) {
-				break;
-			}
+				System.out.println("girilen sayı tektir =" + odd);
 
-		}
+			}
+		} while (odd % 2 == 0);
 		System.out.println("işlem bitti toplam= " + sum);
 
 	}
