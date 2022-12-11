384a924da95c775480784637f90ba7f9e810f243
Aydın Demir <aydndenr@gmail.com>
2022-12-11 13:04:07 +0300

    Create ODEV3.sql

---
+++ Postgresql/ODEV3.sql
@@ -0,0 +1,4 @@
+select * from country where country like 'A%a'
+select * from country where country like '_____%n'
+select film from film where title like'%t%t%t%t%'
+select * from film where title ilike 'C%' AND length>90 and rental_rate=2.99
