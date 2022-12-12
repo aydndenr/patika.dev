09d370b98a147296112737a21ece55b4e9be6bd1
Aydın Demir <aydndenr@gmail.com>
2022-12-13 00:06:56 +0300

    Create Nnumber.java

---
+++ Nnumber.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Nnumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+
+		int numb1, numb2 = 0, numb3 = 0, changes;
+		System.out.println("KAÇ SAYI GİRİLECEKTİR?= ");
+		changes = scan.nextInt();
+		if (changes < 1) {
+			System.out.println("böyle bir işlem yapılamas");
+			return;
+		}
+		while (changes > 0) {
+
+			System.out.println("SAYI GİRİNİZ ?= ");
+			numb1 = scan.nextInt();
+			if (changes < 2) {
+				System.out.println(changes < 2 ? "tek sayı girildiğinden en küçük ve enbüyük sayıdır  " + numb1
+						: " burada olmaması gerekir?");
+				return;
+			}
+
+			--changes;
+			if (numb1 > numb3)
+				numb3 = numb1;
+			else if (numb1 < numb2)
+				numb2 = numb1;
+			else
+				continue;
+
+		}
+		System.out.println("en büyük sayı =" + numb3);
+		System.out.println("en küçük sayı =" + numb2);
+
+	}
+
+}
